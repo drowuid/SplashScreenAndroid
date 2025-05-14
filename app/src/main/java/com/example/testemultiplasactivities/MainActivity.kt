8f3ff4738baed4a97d778a83ab1c24363ba327ab
Pedro <pmprodrigueswork@gmail.com>
2025-05-14 11:55:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/testemultiplasactivities/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.testemultiplasactivities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.testemultiplasactivities.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.buttonSaltar.setOnClickListener {
+
+            val intent = Intent(this,SegundaActivity::class.java)
+            startActivity(intent)
+            //startActivity(Intent(this,SegundaActivity::class.java))
+
+        }
+    }
+}
